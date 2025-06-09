9657f1da993f0419386dd0eed13690c13599e3b5
Pedro Henrique de Assis Ferreira <73146680+FerreiraPedroo@users.noreply.github.com>
2025-06-08 22:02:56 -0300

    update

--- src/main/java/br/com/alura/ScreenMatch/config/CorsConfiguration.java
+++ src/main/java/br/com/alura/ScreenMatch/config/CorsConfiguration.java
@@ -1,11 +1,16 @@
 package br.com.alura.screenmatch.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedOrigins("http://172.26.192.1:5500")
+		.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+	}
 }
